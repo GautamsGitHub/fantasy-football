4be5d5d0d5aa1298cdb79cd9515811b6bcac8692
Gautam <1gautamj@gmail.com>
2023-10-11 18:54:40 +0100

    allow heroku host

--- Fantasy/settings.py
+++ Fantasy/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['icuafc-ff-8428aeb8e9d2.herokuapp.com']
 
 
 # Application definition
