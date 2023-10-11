2cb1df6a88b0a2f726f8bd7d4f243b6ba8ce56e0
Gautam <1gautamj@gmail.com>
2023-10-11 19:13:22 +0100

    change the database url

--- Fantasy/settings.py
+++ Fantasy/settings.py
@@ -82,11 +82,9 @@ WSGI_APPLICATION = 'Fantasy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 
